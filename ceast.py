d17f0d7b1f9336f43e86cbd0bf72492ea046d9ef
ECTOBIOLOBAJUN <47796424+ECTOBIOLOBAJUN@users.noreply.github.com>
2019-05-06 11:27:33 +0300

    Add files via upload

--- ceast.py
+++ ceast.py
@@ -1,11 +1,17 @@
-print("enter your key(number)")
-key = int(input())
+
+
+print("enter your key(word)")
+key = input()
 print("enter your very improtant data")
 string = input()
-print("your encrypted data is ",end="")
-for i in range(len(string)) :
+key = key*(len(string)//len(key)+1)
+print("your encrypted data is ", end="")
+for i in range(len(string)):
     if string[i] != " ":
-        b=chr((ord(string[i])-96+key)%26+96)
-        if b =="`":print("z",end="")
-        else :print(b,end="")
-    else: print(" ",end="")
+        b = chr((ord(string[i])-96+(ord(key[i])-97)) % 26 + 96)
+        if b == "`":
+            print("z", end="")
+        else:
+            print(b, end="")
+    else:
+        print(" ", end="")
