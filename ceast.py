d412340f541f644c238f6b701fc3ddba17a4fc2c
ECTOBIOLOBAJUN <47796424+ECTOBIOLOBAJUN@users.noreply.github.com>
2019-05-06 11:29:19 +0300

    Update ceast.py

--- ceast.py
+++ ceast.py
@@ -1,17 +1,16 @@
 
-
 print("enter your key(word)")
 key = input()
 print("enter your very improtant data")
 string = input()
-key = key*(len(string)//len(key)+1)
-print("your encrypted data is ", end="")
+key = key * (len(string) // len(key) + 1)
+print("your encrypted data is ", end = "")
 for i in range(len(string)):
     if string[i] != " ":
-        b = chr((ord(string[i])-96+(ord(key[i])-97)) % 26 + 96)
+        b = chr((ord(string[i]) - 96 + (ord(key[i]) - 97)) % 26 + 96)
         if b == "`":
-            print("z", end="")
+            print("z", end = "")
         else:
-            print(b, end="")
+            print(b, end = "")
     else:
-        print(" ", end="")
+        print(" ", end = "")
